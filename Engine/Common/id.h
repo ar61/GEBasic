a07f6ec959f9db3ee95ce87bfb5bb6a9e7953b2e
Abhinav Rathod <abhinav.rathod@fove-inc.com>
2024-05-06 18:22:54 -0400

    Id setup is done

--- Engine/Common/id.h
+++ Engine/Common/id.h
@@ -8,13 +8,35 @@ namespace gebasic::id {
 
 	using id_type = u32;
 
-	constexpr u32 generation_bits{ 8 };
-	constexpr u32 index_bits{ 8 * sizeof(id_type) - generation_bits };
-	constexpr id_type index_mask{ (id_type{1} << index_bits) - 1 }; // 0111
-	constexpr id_type generation_mask{ (id_type{1} << generation_bits) - 1 }; // 1000
+	constexpr u32 generation_bits{ 8 }; // 0000 0000 0000 0000 0000 0000 0000 1000
+	constexpr u32 index_bits{ 8 * sizeof(id_type) - generation_bits }; // 0000 0000 0000 0000 0000 0000 0001 1000
+	constexpr id_type index_mask{ (id_type{1} << index_bits) - 1 };
+	constexpr id_type generation_mask{ (id_type{1} << generation_bits) - 1 }; // 0111 1111
 	constexpr id_type id_mask{ id_type{-1} };
 
 	using generation_type = std::conditional_t<generation_bits <= 16, std::conditional_t<generation_bits <= 8, u8, u16>, u32>;
 	static_assert(sizeof(generation_type) * 8 >= generation_bits);
+	static_assert(sizeof(id_type) - sizeof(generation_type) > 0);
 
+	inline bool
+	isValid(id_type id) {
+		return id != id_mask;
+	}
+
+	inline id_type
+	index(id_type id) {
+		return id & index_mask;
+	}
+
+	inline id_type
+	generation(id_type id) {
+		return (id >> index_bits) & generation_mask;
+	}
+
+	inline id_type
+	newGeneration(id_type id) { // ex: id = 1001 
+		const id_type generation{ id::generation(id) + 1 }; // 0001 + 1 => 0002
+		assert(generation < 255);
+		return index(id) | (generation << index_bits); // 1001 | 2000 => 2001
+	}
 }
